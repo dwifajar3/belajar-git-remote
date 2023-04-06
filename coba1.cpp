4535bc2ee3b54c36bae1cd414c8feac59fb002aa
Fajar Dwi Cahyo <129813531+dwifajar3@users.noreply.github.com>
2023-04-06 10:34:45 +0700

    Update coba1.cpp

--- coba1.cpp
+++ coba1.cpp
@@ -10,6 +10,15 @@ int main(){
     for(int i=0;i<5;i++){
         cout<< "Fajar Dwi"; cout << endl;
     }
+    
+    cout << endl;
+    
+    int a;
+    cout << "masukan nilai : "; cin >> a;
+    
+    while(a=10){
+        cout << "fajar"; cout<< endl;
+    }
 
 
 }
